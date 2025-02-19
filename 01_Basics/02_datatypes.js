c67688f923ce2af7675224e870183574cd6c1a36
UnzaShaikh <83079287+UnzaShaikh@users.noreply.github.com>
2025-02-19 11:01:29 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"  /* treat all the code as a newer version, its not nesecerry to wirte this becuase the engine will automatically understands this but 
+to follow best practices */
+
+// alert(2+2) works on brower not with node js 
+
+// Data types in JS
+/* Number 
+string
+bigint - for large number value
+boolean true/false
+undefined - means the variable is not initilalized yet and its data type is undefined.
+null -  its datatype is object but it has not an special data type assigend just like undefined, null means initialiy variable does not contain
+any value but it might possible that in future it will be initialized with some data.
+symbol - for unique value 
+objects
+
+*/
+let state = null;
+console.log(typeof(state));  // to know any datatype of variable
+
