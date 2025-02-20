e42d2a048a58572ec84e5e2e8a4805e80524d2db
UnzaShaikh <83079287+UnzaShaikh@users.noreply.github.com>
2025-02-20 12:14:11 +0000

    Discussion about operators

--- 01_Basics/03_conversionoperations.js
+++ 01_Basics/03_conversionoperations.js
@@ -31,8 +31,8 @@ undefiend                     undefined    string
 let isOff=123 ;
 
 let stringIsOff = Boolean(isOff);
-console.log(typeof stringIsOff);
-console.log(stringIsOff);
+// console.log(typeof stringIsOff);
+// console.log(stringIsOff);
 
 // conversion results 
 /*                              value    typeconversion
@@ -44,3 +44,30 @@ undefiend                        false      boolean
 "lion"                           true       boolean
 123                              true       boolean
 */
+
+/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Operations >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**2); Power
+console.log(2%2); Modulus
+
+<<<<<<<<<<<<<<< Bad code habits >>>>>>>>>>>>>
+const str1 = "1" + "2"  // 12
+const str2 = 1 + "2"  // 12
+const str3 = 1 + 2  // 3
+const str4 = 1 + 2 + "2"  // 32  -- if string is in last place than the values at the front will be added
+const str5 = "1" + 2 + 2  // 122 -- if string is in front place than the values at the last will not be added
+const = +true // 1
+const = +"" // 0
+const num1 = num2 = num 3 = 2 + 2  // plz dont code like this 
+*/
+// let gameCounter = 100;
+// let currGameCounter = gameCounter++; // post -- the increment operator increments and returns the value before incrementing.
+// console.log(`gameCounter:${gameCounter} and currGameCounter: ${currGameCounter}`);
+
+let gameCounter = 100;
+let currGameCounter = ++gameCounter; // pre -- the increment operator increments and returns the value after incrementing.
+console.log(`gameCounter:${gameCounter} and currGameCounter: ${currGameCounter}`);
